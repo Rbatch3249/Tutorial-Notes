d4bc07be4d6c042eb4f62b71c04531267591f7b9
Ryan <rbatchelor3249@gmail.com>
2021-03-18 14:35:14 -0400

    skdjfa

---
+++ src/main/java/MySweetProgram/MySweetProgram.java
@@ -0,0 +1,37 @@
+package MySweetProgram;
+
+import java.util.Scanner;
+
+public class MySweetProgram {
+
+
+    public static void main(String[] args) {
+    double x = 5.0 / 2;
+        System.out.println(x);
+
+    }
+
+}
+
+//variable create
+//1. declare
+//2. initialize
+
+
+//primitive types - boolean, byte, char, short, int, long, float, double
+//objects - instance of a class (class the data type)
+//statically typed =variables are given data types up front
+// dynamically typed = variables do not have types
+//data type = the type of the dat. how the computer interprets the data
+//literals - the value
+//variable - stores some value
+//expression -evaluates to a value
+//operator - worn on operands to produce a value
+//operand - the thing(s) the operator works on
+//class - contains everything -has members
+//methods - do something
+//statement - telling the computer something
+//properties - store something
+//access modifier (public) - who can use?
+//static - no instance of class is needed
+//object - instance of a class
